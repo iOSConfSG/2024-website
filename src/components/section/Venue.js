cb3df44a96e30aff17fe3815b7c0bd115c6b9042
Subhransu Behera <subhransu@spdigital.sg>
2023-11-22 16:16:59 +0800

    Update Discord Link

--- src/components/section/Venue.js
+++ src/components/section/Venue.js
@@ -24,7 +24,7 @@ export default function VenueSection () {
               </div>
           </div>
           <div className="mt-12 lg:mt-0 lg:col-start-1">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7977.529974514071!2d103.8836639086914!3d1.3165989010852905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da19d772eeca0b%3A0x87ceaa7082d263d9!2sSingPost%20Centre!5e0!3m2!1sen!2ssg!4v1698242423690!5m2!1sen!2ssg" width={"100%"} height={500} style={{ border: 0 }} allowfullscreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7977.529974514071!2d103.8836639086914!3d1.3165989010852905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da19d772eeca0b%3A0x87ceaa7082d263d9!2sSingPost%20Centre!5e0!3m2!1sen!2ssg!4v1698242423690!5m2!1sen!2ssg" width={"100%"} height={500} style={{ border: 0 }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
           </div>
           </div>
       </div>
